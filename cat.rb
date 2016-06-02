120b37a6d3a7217949de6c9ba9b8dea526c33c0e
chau nguyen <nhbc111@yahoo.com>
2016-06-02 13:21:06 -0400

    new exercise

---
+++ cat.rb
@@ -0,0 +1,16 @@
+class Student
+  def initialize(name)
+    @name = name
+  end
+  def learn
+    "I get it!"
+  end
+end
+
+class Instractor
+  def initilize(name)
+  end
+  def teach
+    "Everything in Ruby is an Object."
+  end
+end
