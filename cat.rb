b0a8a5c35d2321d663ac8af3dcc62c6c33a05874
chau nguyen <nhbc111@yahoo.com>
2016-06-02 14:49:23 -0400

    added something

--- cat.rb
+++ cat.rb
@@ -1,9 +1,21 @@
 class Cat
   attr :name, :preffered_food, :meal_type
-  def initialize(name, preffered_food, meal_type)
+  def initialize(name, preffered_food, meal_time)
     @name = name
     @preffered_food = preffered_food
-    @meal_type = meal_type
+    @meal_time = meal_time
+  end
+
+  def name
+    @name
+  end
+
+  def preffered_food
+    @preffered_food
+  end
+
+  def meal_time
+    @meal_time
   end
 end
 #
